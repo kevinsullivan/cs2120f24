0d79f7cb1f33bab466411c0565563690b542f1d9
Kevin Sullivan <sullivan@virginia.edu>
2024-09-08 19:17:48 -0400

    .

--- Cs2120f24.lean
+++ Cs2120f24.lean
@@ -1,9 +1,6 @@
 -- This module serves as the root of the `Cs2120f24` library.
 -- Import modules here that should be built as part of the library.
 
--- Unit 1: Languages, Natural and Formal
-import «Cs2120f24».Lectures.«01_languages».declararive_example
-
 -- Unit 2: Propositional Logic
 import «Cs2120f24».Lectures.«02_prop_logic».formal.syntax
 import «Cs2120f24».Lectures.«02_prop_logic».formal.domain
