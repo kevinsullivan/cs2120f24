9a3683204047c450cc9324484341196671b9714e
Kevin Sullivan <sullivan@virginia.edu>
2024-09-12 23:57:42 -0400

    Add explanation.

--- Cs2120f24/Lectures/02_prop_logic/formal/models_counterexamples.lean
+++ Cs2120f24/Lectures/02_prop_logic/formal/models_counterexamples.lean
@@ -217,4 +217,10 @@ together a method for finding a counterexample if there is one?
 def findCounterexamples (e : PLExpr) : List BoolInterp := findModels ¬e
 def findCounterexample (e : PLExpr) : Option BoolInterp := findModel ¬e
 
+/-!
+These functions use types you don't yet know about: namely List and Option.
+You should understand lists intuitively from CS1. You can think of an option
+as a list of length either zero (called none) or one (called some e), where
+e the specific value in the length-one list of values (an interpertation).
+-/
 end cs2120f24
