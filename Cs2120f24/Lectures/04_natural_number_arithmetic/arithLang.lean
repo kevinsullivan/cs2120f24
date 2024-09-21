d54b3ded81409e7594ba4263f1ec0cb4bbcd54c1
Kevin Sullivan <sullivan@virginia.edu>
2024-09-20 22:25:18 -0400

    FInishing up refactoring.

--- Cs2120f24/Lectures/04_natural_number_arithmetic/arithLang.lean
+++ Cs2120f24/Lectures/04_natural_number_arithmetic/arithLang.lean
@@ -1,21 +1,25 @@
 /-!
 An expression language for natural number arithmetic all in one place,
 with homework on completing the syntax and semantics of this language.
-
 The evolved and refined version of this code in Library/natArithmetic.
+
 There we explore the domain by deliving into the Nat type, its simple
 induction principle, and an expression language for this domain. We're
 on a path to incorporating arithmetical relational expressions into
 our currently bare version of propositional logic. That will give us a
 wonderful version of proposition logic with arithmetic relations as a
-theory extension. That will take us to the end of our adventure in the
-wilds of formal language design. We will not, however, develop our own
-model theory for the artithmetic component. Rather, we'll then turn to
-a real-world "satisfiability modulo theories" solver that *can* solve
-systems of arithmetic constraints. Really understanding what we can do
-with a language and systen for PL with Natural Number Arithmetic should
-give you a sense of the rapidly increasing expressiveness of logics we
-can now envision and specify.
+theory extension.
+
+That will take us to the end of our adventurea in formal language design.
+We will not develop our own model theory for the artithmetic component.
+That would involve solving natural number algebra problems.
+
+Rather, we'll then turn to a real-world "satisfiability modulo theories"
+language and solver that *can* solve systems of arithmetic constraints.
+Understanding what we can do with a language and systen for PL extended
+with a theory of natural number arithmetic should give you a sense of the
+rapidly increasing expressiveness of logics we can envision, specify, and
+use.
 -/
 
 structure ArithVar : Type :=
---
+++ Cs2120f24/Library/natArithmetic/induction.md
@@ -0,0 +1,3 @@
+# Note
+
+The induction.lean file has been moved to Lectures/04_natural_number_arithmetic
